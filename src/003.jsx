1fad7d575d6fa09f06b2dad9d259c89cea754ac1
NerijusBun <nerijuz@gmail.com>
2023-05-17 22:29:48 +0300

    update

---
+++ src/003.jsx
@@ -0,0 +1,23 @@
+import './App.scss';
+import Inputs from './Components/003/Inputs';
+//import Ls from './Components/003/Ls';
+//import Sq4 from './Components/003/Sq4';
+import './buttons.scss';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        { /* <Sq4 /> */}
+
+        {/* <Ls /> */}
+        <Inputs />
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
